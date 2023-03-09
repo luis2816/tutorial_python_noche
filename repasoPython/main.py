46577b3f96376764b89bf82adc837150e7bd3e00
luis2816 <ordonezfelipe2816@gmail.com>
2023-03-08 20:26:19 -0500

    Se agrega los operadores

--- repasoPython/main.py
+++ repasoPython/main.py
@@ -23,4 +23,53 @@ mensaje= "Cadena con una comilla simple ', una comilla doble \" y una diagonal i
 print(mensaje)
 
 
+# Opreradores
+#Aritmeticos
+
+numero5= 78
+numero6= 54
+suma= numero5 + numero6
+resta=numero5 - numero6
+multiplicacion= numero5 * numero6
+divicion = numero5 / numero6
+modulo=numero5 % numero6
+
+print("operadores Aritmeticos")
+print("La suma es: ", suma)
+print("La resta es: ", resta)
+print("La multiplicacion es: ", multiplicacion)
+print("La divicion es: ", divicion)
+print("El modulo es: ", modulo)
+
+#Asignación
+x= 5
+y= 6
+z= 8
+
+#Logicos
+#and (y)
+
+p=5
+q= 9
+print(p < 7 and  q < 9)
+print("operadores de logicos")
+# or (o)
+
+print(p < 7 or  q < 9)
+
+#not
+
+print(not( q > 2 and p < 7))
+
+#Relacionales
+edad1= 23
+edad2= 18
+print("Opreradores Relacionales")
+print(edad1 == edad2) # gualdad
+print(edad1 > edad2) # Mayor que
+print(edad1 < edad2) # Menor que
+print(edad1 >= edad2) # Mayor o igual que
+print(edad1 <= edad2) # Mayor o igual que
+print(edad1 != edad2) # No igual
+
 
