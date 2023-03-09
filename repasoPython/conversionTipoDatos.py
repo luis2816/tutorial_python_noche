511d3847d63e7660c580a9ab6cbffb56e570dce3
luis2816 <ordonezfelipe2816@gmail.com>
2023-03-09 15:33:08 -0500

    Se agregan conversion de tipos de datos

---
+++ repasoPython/conversionTipoDatos.py
@@ -0,0 +1,51 @@
+#Convertir un número entero a un número flotante:
+num_entero = 30
+num_flotante = float(num_entero)
+print("el numero entero es = ", num_entero, " el numero convertido a flotante es = ", num_flotante)
+print("finalizando conversion de numero entero  a flotante")
+print("#################################################################")
+
+
+#Convertir un número flotante a un número entero:
+print("Inicializando conversion de numero flotante  a entero")
+num_flotante = 50.7
+num_entero = int(num_flotante)
+print("el numero flotante es = ", num_flotante, " el numero convertido a entero es = ", num_entero)
+print("Finalizando conversion de numero flotante  a entero")
+print("#################################################################")
+
+#Convertir un numero a una cadena de texto
+print("Inicializando conversion de numero a cadena de texto")
+numero = 50
+cadena = str(numero)
+print("el numero es = ", numero , "el numero convertido a cadena de texto es " + cadena)
+print("Finalizando conversion de numero a cadena de texto")
+print("#################################################################")
+
+
+#Convertir una cadena de texto a un número entero
+print("Inicializando conversion de cadena de texto a entero")
+cadena = "12345"
+numInt = int(cadena)
+print("La cadena de texto  es = " + cadena + " el numero convertido a entero es = ", numInt)
+print("Finalizando conversion cadena de texto a entero")
+print("#################################################################")
+
+#Convertir una cadena de texto a un número flotante
+print("Inicializando conversion de cadena de texto a flotante")
+cadena = "123.456"
+numFloat = float(cadena)
+print("La cadena de texto  es = " + cadena + " el numero convertido a flotante es = ", numFloat)
+print("Finalizando conversion cadena de texto a e Flotante")
+print("#################################################################")
+
+
+#Convertir un valor booleano a un entero:
+print("Inicializando conversion de booleano  a entero")
+valor_booleano = True
+numBolleano = int(valor_booleano)
+print("El balor booleano es = ", valor_booleano, "la conversión a entero es = ", numBolleano )
+print("Finalizando conversion booleano a entero")
+print("#################################################################")
+
+
