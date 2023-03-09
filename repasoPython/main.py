f0be93380a50815df35836bf6aa479683d3358fe
luis2816 <ordonezfelipe2816@gmail.com>
2023-03-08 19:53:49 -0500

    Se agregan los Tipos de datos en Python

---
+++ repasoPython/main.py
@@ -0,0 +1,26 @@
+# Tipos de datos
+# Numeros enteros
+print("tipo de datos  primitivos")
+numero1: int = 60
+numero2=45
+print("Numeros enteros")
+print(numero1)
+print(numero2)
+print("Numeros reales")
+#Numeros Reales
+numero3: float = 4.9
+numero4= 5.1
+print(numero3)
+print(numero4)
+#Numeros Booleanos
+esColombiano: bool = True
+esArgentina: bool = False
+print("Numeros Booleanos toman dos valores Verdadero/falso")
+print(esColombiano)
+print(esArgentina)
+#Caracter  y cadena de caracteres
+mensaje= "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida \\"
+print(mensaje)
+
+
+
